6204ebb25449831df63d1ae07dae24f233567bcf
Jim van Zuidam <jimvanzuidam@hotmail.nl>
2018-12-10 10:59:58 +0100

    New Angular

--- Client/server.js
+++ Client/server.js
@@ -16,7 +16,7 @@ app.use(compression());
 //
 // Replace the name below to match your own "defaultProject" value!
 //
-const appname = 'angular-heroku';
+const appname = 'Client';
 
 // Point static path to dist
 app.use(express.static(path.join(__dirname, 'dist', appname)));
--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -3,14 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { UserComponent } from './user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    DashboardComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
