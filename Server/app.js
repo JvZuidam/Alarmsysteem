dc86609ae6d1a119125c0a59b1dc56fcbaff7b0d
Jim van Zuidam <jimvanzuidam@hotmail.nl>
2018-12-10 23:29:17 +0100

    production

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 3000;
 // const db = require("./dbCon");
@@ -8,6 +9,7 @@ const envVar = 'production';
 app.use("/user", require("./routes/user"));
 app.use("/camera", require("./routes/camera"));
 app.use("/alarm", require("./routes/alarm"));
+app.use(cors());
 
 app.get("*", (request, result) => {
     result.status(404);
