e5944a8d4da7a52ef6c3786165fc2fc130ba64a7
Jim van Zuidam <jimvanzuidam@hotmail.nl>
2018-12-11 09:29:30 +0100

    cors update server camera & alarm

--- Server/routes/alarm.js
+++ Server/routes/alarm.js
@@ -8,6 +8,12 @@ const responseMessages = require("../responseMessages");
 router.use(bodyParser.json()); // support json encoded bodies
 router.use(bodyParser.urlencoded({extended: true}));
 
+router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
+
 //Create alarm
 router.post("", (request, result) => {
     const userName = request.body.username;
