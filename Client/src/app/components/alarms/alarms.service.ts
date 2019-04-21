a2c365705aa6f6094664931464b34e4a9662be41
Jim van Zuidam <j.vanzuidam@student.avans.nl>
2019-04-22 00:28:23 +0200

    Client: Expended the edit and create pages of cameras

--- Client/src/app/components/alarms/alarms.service.ts
+++ Client/src/app/components/alarms/alarms.service.ts
@@ -3,30 +3,36 @@ import { Alarm } from './alarm.model';
 import { HttpClient } from '@angular/common/http';
 import {map, tap} from 'rxjs/operators';
 import {User} from '../users/user.model';
+import {Camera} from '../cameras/camera.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AlarmsService {
 
+  alarm: Alarm[];
+
   constructor(
     private http: HttpClient) {
 
   }
-  public createAlarms() {
-
+  public createAlarms(alarm: Alarm, user: string, camera: string) {
+    return this.http.post<any>('https://alarmsysteem-server.herokuapp.com/alarm', {
+      "username" : user,
+      "cameraName" : camera,
+      "alarmName" : alarm.title,
+      "description" : alarm.description,
+      "alarmType" : alarm.alarmType,
+      "alarmLevel" : alarm.alarmLevel
+    })
   }
 
-  public getAlarms() {
+  public getAlarms(user: string, camera: string) {
     return this.http.get<any>('https://alarmsysteem-server.herokuapp.com/alarm/Jim/Camera2/5c0f77c1603eb3001649685b')
       .pipe(
         tap(response => console.log(response.results)),
         map(response => response.results.map(data => new User(data)))
       );
-  }
-
-  public updateAlarms() {
-
   }
 
   public deleteAlarms() {
